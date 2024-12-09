bac3a5e14babeb14e4b156a5b25de5ef08dc2a0c
Arun Kumar Madesh <56151567+arunkumar461@users.noreply.github.com>
2024-12-09 10:20:17 +0000

    refactor button component1

--- src/main/java/com/moj/thymeleafexperiment/ButtonElementTagProcessor.java
+++ src/main/java/com/moj/thymeleafexperiment/ButtonElementTagProcessor.java
@@ -26,58 +26,90 @@ public class ButtonElementTagProcessor extends AbstractElementTagProcessor {
     @Override
     protected void doProcess(ITemplateContext context, IProcessableElementTag tag, IElementTagStructureHandler structureHandler) {
 
+        Map<String, String> attributes = parseAttributes(context, tag);
+        String classNames = buildClassNames(attributes);
+        String commonAttributes = buildCommonAttributes(classNames, attributes);
+        String buttonAttributes = buildButtonAttributes(attributes);
+
+        String html = attributes.containsKey("href")
+                ? buildAnchorHtml(attributes, commonAttributes)
+                : buildButtonHtml(attributes, commonAttributes, buttonAttributes);
+
+        replaceElementWithHtml(context, structureHandler, html);
+    }
+
+    private Map<String, String> parseAttributes(ITemplateContext context, IProcessableElementTag tag) {
         Map<String, String> attributes = tag.getAttributeMap();
-        Map<String, String> params = new HashMap<>();
+        Map<String, String> resolvedAttributes = new HashMap<>();
         IStandardExpressionParser parser = StandardExpressions.getExpressionParser(context.getConfiguration());
+
         for (Map.Entry<String, String> entry : attributes.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
             if (key.startsWith("th:")) {
                 IStandardExpression expression = parser.parseExpression(context, value);
-                String resolvedValue = (String) expression.execute(context);
-                params.put(key.replace("th:", ""), resolvedValue);
+                resolvedAttributes.put(key.replace("th:", ""), (String) expression.execute(context));
             } else {
-                params.put(key, value);
+                resolvedAttributes.put(key, value);
             }
         }
 
-        String classNames = "govuk-button";
-        if (params.containsKey("classes")) {
-            classNames += " " + params.get("classes");
-        }
+        return resolvedAttributes;
+    }
 
+    private String buildClassNames(Map<String, String> attributes) {
+        String classNames = "govuk-button";
+        if (attributes.containsKey("classes")) {
+            classNames += " " + attributes.get("classes");
+        }
+        return classNames;
+    }
+
+    private String buildCommonAttributes(String classNames, Map<String, String> attributes) {
         StringBuilder commonAttributes = new StringBuilder();
         commonAttributes.append("class=\"").append(classNames).append("\" data-module=\"govuk-button\"");
-
-        if (params.containsKey("id")) {
-            commonAttributes.append(" id=\"").append(params.get("id")).append("\"");
+        if (attributes.containsKey("id")) {
+            commonAttributes.append(" id=\"").append(attributes.get("id")).append("\"");
         }
+        return commonAttributes.toString();
+    }
 
+    private String buildButtonAttributes(Map<String, String> attributes) {
         StringBuilder buttonAttributes = new StringBuilder();
-        if (params.containsKey("name")) {
-            buttonAttributes.append(" name=\"").append(params.get("name")).append("\"");
+        if (attributes.containsKey("name")) {
+            buttonAttributes.append(" name=\"").append(attributes.get("name")).append("\"");
         }
-        if (params.containsKey("disabled")) {
+        if (attributes.containsKey("disabled")) {
             buttonAttributes.append(" disabled aria-disabled=\"true\"");
         }
-        if (params.containsKey("preventDoubleClick")) {
-            buttonAttributes.append(" data-prevent-double-click=\"").append(params.get("preventDoubleClick")).append("\"");
+        if (attributes.containsKey("preventDoubleClick")) {
+            buttonAttributes.append(" data-prevent-double-click=\"").append(attributes.get("preventDoubleClick")).append("\"");
         }
+        return buttonAttributes.toString();
+    }
 
-        StringBuilder html = new StringBuilder();
-        if (params.containsKey("href")) {
-            html.append("<a href=\"").append(params.getOrDefault("href", "#")).append("\" role=\"button\" draggable=\"false\" ").append(commonAttributes).append(">");
-            html.append(params.getOrDefault("text", ""));
-            html.append("</a>");
-        } else {
-            html.append("<button type=\"").append(params.getOrDefault("type", "submit")).append("\" ").append(buttonAttributes).append(" ").append(commonAttributes).append(">");
-            html.append(params.getOrDefault("text", ""));
-            html.append("</button>");
-        }
+    private String buildAnchorHtml(Map<String, String> attributes, String commonAttributes) {
+        return new StringBuilder()
+                .append("<a href=\"").append(attributes.getOrDefault("href", "#"))
+                .append("\" role=\"button\" draggable=\"false\" ").append(commonAttributes).append(">")
+                .append(attributes.getOrDefault("text", ""))
+                .append("</a>")
+                .toString();
+    }
 
+    private String buildButtonHtml(Map<String, String> attributes, String commonAttributes, String buttonAttributes) {
+        return new StringBuilder()
+                .append("<button type=\"").append(attributes.getOrDefault("type", "submit"))
+                .append("\" ").append(buttonAttributes).append(" ").append(commonAttributes).append(">")
+                .append(attributes.getOrDefault("text", ""))
+                .append("</button>")
+                .toString();
+    }
+
+    private void replaceElementWithHtml(ITemplateContext context, IElementTagStructureHandler structureHandler, String html) {
         final IModelFactory modelFactory = context.getModelFactory();
-        final IModel model = modelFactory.parse(context.getTemplateData(), html.toString());
-
+        final IModel model = modelFactory.parse(context.getTemplateData(), html);
         structureHandler.replaceWith(model, false);
     }
 }
+
